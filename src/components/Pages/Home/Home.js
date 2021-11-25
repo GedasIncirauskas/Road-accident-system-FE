603811cb9d2718e445fdecf8c62cb55a5484b294
GedasIncirauskas <gedas.inc@gmail.com>
2021-11-25 20:44:16 +0200

    added chart

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Spinner } from '../../';
 import * as S from './Home.styles';
+import Chart from '../../Chart/Chart';
 
 const Home = () => {
   return (
     <S.ContainerStyle>
-      <Spinner />
+      {/* <Spinner /> */}
+      <Chart />
     </S.ContainerStyle>
   );
 };
