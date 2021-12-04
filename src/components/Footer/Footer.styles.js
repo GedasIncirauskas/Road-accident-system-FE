f3b577bba0520e8b32390b4597b80a72bb17157a
GedasIncirauskas <gedas.inc@gmail.com>
2021-12-04 11:56:52 +0200

    created footer

---
+++ src/components/Footer/Footer.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const FooterStyles = styled.footer`
+  background-color: rgb(241, 241, 241);
+  height: 3rem;
+  width: 100%;
+  line-height: 3rem;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  z-index: 1000;
+
+  @media screen and (max-width: 500px) {
+    height: 2rem;
+    width: 100%;
+    line-height: 2rem;
+    font-size: 0.6rem;
+  }
+`;
