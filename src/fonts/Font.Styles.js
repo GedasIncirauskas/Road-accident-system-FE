edfcae29e7313c2acd5fd52a52861cc8d9143903
GedasIncirauskas <gedas.inc@gmail.com>
2021-12-05 17:27:10 +0200

    add fonts

---
+++ src/fonts/Font.Styles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const FontStyles = createGlobalStyle`
+body {
+    font-family: 'Manrope', sans-serif;
+}
+`;
+
+export default FontStyles;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CoordinateProvider from './contexts/Coordinates';
+import FontStyles from './fonts/Font.Styles';
 import 'normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <CoordinateProvider>
+      <FontStyles />
       <App />
     </CoordinateProvider>
   </React.StrictMode>,
